8884048117da4c2aae6a34db9ad7b2fa18022cea
Olatunji S <teejayx@outlook.com>
2023-04-01 00:40:38 +0100

    unit test

---
+++ MauitTest.Test/PhoneWordTranslatorTest.cs
@@ -0,0 +1,21 @@
+﻿using Service;
+
+namespace MauitTest.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldConvertWordsToText()
+    {
+        //Arrange
+         string phoneWord = "1-555-NETMAUI";
+        string expectedNumber = "1-555-6386284";
+        //Act
+
+        var actualNumber = PhoneWordTranslator.ToNumber(phoneWord);
+
+        //Assert
+
+        Assert.Equal(actualNumber, expectedNumber);
+    }
+}
