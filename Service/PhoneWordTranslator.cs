a2a71d1fe7fd4d211a83fd2cdd3502fabcb05d6d
Olatunji S <teejayx@outlook.com>
2023-03-31 23:54:01 +0100

    Convert phoneword to numbers

--- Service/PhoneWordTranslator.cs
+++ Service/PhoneWordTranslator.cs
@@ -7,29 +7,28 @@ namespace Service
 	{
 		public static string ToNumber(string raw)
 		{
-			if (string.IsNullOrWhiteSpace(raw))
-				return null;
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
 
+            raw = raw.ToUpperInvariant();
 
-			raw = raw.ToUpperInvariant();
-			var newNumber = new StringBuilder();
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                    // Bad character?
+                    else
+                        return null;
+                }
+            }
+            return newNumber.ToString();
 
-			foreach (var item in raw)
-			{
-				if (" -0123456789".Contains(item))
-					newNumber.Append(raw);
-				else
-				{
-					var result = TranslateToNumber(item);
-					if (result != null)
-						newNumber.Append(result);
-
-					else
-						return null;
-				}
-
-			}
-			return newNumber.ToString();
         }
 
 
